c04dc8a9acc4648d52ab124aa761e94363bf579c
VaisakhPushpan <112495591+VaisakhPushpan@users.noreply.github.com>
2024-02-21 16:33:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {  HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { Provider } from "react-redux";
 import { MyStore } from "./redux/MyStore.jsx";
-import {  HashRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={MyStore}>
